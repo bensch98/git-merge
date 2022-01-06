69cbd948c802cc6489921a9d0374025b89c92ce5
bensch98 <scheffler.benedikt@googlemail.com>
2022-01-06 23:06:22 +0100

    check for incompatible CLI arguments

--- gitmerge/errors.py
+++ gitmerge/errors.py
@@ -4,9 +4,19 @@ class MissingArgumentException(Exception):
     :param missing_args: List of argument names, that are missing.
     """
     self.missing_args = missing_args
-    self.message = self.__construct_message()
-    super(MissingArgumentException, self).__init__(self.message)
+    self.msg = self.__construct_message()
+    super(MissingArgumentException, self).__init__(self.msg)
   
   def __construct_message(self):
     msg = f'Following argumets are missing: {", ".join(self.missing_args)}'
+    return msg
+
+class IncompatibleArgumentsException(Exception):
+  def __init__(self, args):
+    self.args = args
+    self.msg = self.__construct_message()
+    super(IncompatibleArgumentsException, self).__init__(self.msg)
+
+  def __construct_message(self):
+    msg = f'Too many arguments specified. One of the following arguments has to be omitted {", ".join(self.args)}'
     return msg
--- gitmerge/gitmerge.py
+++ gitmerge/gitmerge.py
@@ -3,7 +3,7 @@
 import click
 import git
 from gitmerge.merge import Merger, Commit
-from gitmerge.errors import MissingArgumentException
+from gitmerge.errors import MissingArgumentException, IncompatibleArgumentsException
 
 """ CLI """
 
@@ -13,19 +13,22 @@ def gitmerge():
   pass
 
 @click.command(help='Import git commits from one repo to another')
-@click.option('--author', '-a', required=True, help='Name of user which commits of the source repo should be transferred.')
-@click.option('--src', '-s', required=False, help='Path to source repo which the contributions were committed.')
-@click.option('--srcs', required=False, help='Path to a directory, where all subfolders with a .git directory are checked.')
-@click.option('--dest', '-d', required=True, help='Path to destination repo which the contributions should be transferred to.')
-@click.option('--company', '-c', required=True, help='Company name where you at for this repo.')
-@click.option('--since', required=False, default='1w', help='Consider commits since date, days, month, years ago.')
+@click.option('--author', '-a', required=True, help='Name of the user whose commits of the source repo should be transferred.')
+@click.option('--src', '-s', required=False, help='Path to source repo where the commits were created.')
+@click.option('--dir', '_dir', required=False, help='Path to a directory, where all subfolders with a .git directory are considered. This is a shortcut for multiple single statements if you want to transfer commtis of several source repos into your mock repo.')
+@click.option('--dest', '-d', required=True, help='Path to the destination repo where the commits should be transferred to.')
+@click.option('--company', '-c', required=True, help='Company name where you worked at for this repo.')
+@click.option('--since', required=False, default='1w', help='Consider commits since date, days, month, years ago. Examples: 0y3m1w6d, 3m1w6d, 14d, 1d8w.')
 @click.option('--list', '-l', '_list', required=False, is_flag=True, help='Only list the commits, instead of committing and pushing them to the mock repo.')
-def merge(author, src, srcs, dest, company, _list, since):
+def merge(author, src, _dir, dest, company, _list, since):
   
-  # check if arguments are missing
-  if src is None and srcs is None:
-    raise MissingArgumentException(["--src", "--srcs"])
-
+  # check if args are missing or incompatible args where specified
+  if src is None and _dir is None:
+    raise MissingArgumentException(["--src", "--dir"])
+  elif src is not None and _dir is not None:
+    raise IncompatibleArgumentsException(["--src", "--dir"])
+  
+  # init Merger and get commits of repo in time range
   m = Merger(author, src, dest, company)
   commits = m.get_commits(since)
   
