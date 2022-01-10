15fc93849207835185cefcafdd2927862f14b5dc
bensch98 <scheffler.benedikt@googlemail.com>
2022-01-10 21:28:37 +0100

    add unittest for Commit

---
+++ test/test_commit.py
@@ -0,0 +1,27 @@
+import unittest
+import os
+from datetime import datetime
+
+from git import Repo
+from gitmerge.merge import Commit
+
+class TestCommit(unittest.TestCase):
+  
+  def test_init(self):
+    path = os.path.join(os.path.dirname(__file__), '..')
+    repo = Repo(path)
+    self.assertTrue(type(repo) is not None)
+    repo_commits = list(repo.iter_commits('--all'))
+    self.assertTrue(type(repo_commits) == list)
+    
+    # init dummy commits
+    commits = []
+    for c in repo_commits:
+      commits.append(Commit(c.hexsha, datetime.now()))
+
+    for c in commits: 
+      self.assertTrue(c.hexsha is not None)
+      self.assertTrue(c.date is not None)
+
+if __name__ == '__main__':
+  unittest.main()
--- test/test_gitmerge.py
+++
@@ -1,6 +0,0 @@
-import unittest
-import gitmerge
-
-
-if __name__ == '__main__':
-  pass
