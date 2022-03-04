ee917719933ae308f5d8d6ccfb4e31f4b9a0de31
bensch98 <scheffler.benedikt@googlemail.com>
2022-03-04 23:01:30 +0100

    add scripts

---
+++ scripts/docs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ../docs
+make clean html
+make html
--- scripts/push_pypi.sh
+++ scripts/push_pypi.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
+cd ..
 rm -rf dist
 python setup.py sdist bdist_wheel
 twine upload --repository-url https://upload.pypi.org/legacy/ ./dist/*
