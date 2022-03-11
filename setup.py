1ef2a98e953a99333eb2c69c6e40b8341cea36e2
bensch98 <scheffler.benedikt@googlemail.com>
2022-03-11 23:11:56 +0100

    restructure setup

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import pathlib
 
-with open('README.md', 'r') as fh:
+from gitmerge import __version__
+
+
+readme = pathlib.Path(__file__).parent / 'README.md'
+reqs = pathlib.Path(__file__).parent / 'requirements.txt'
+reqs_dev = pathlib.Path(__file__).parent / 'requirements_dev.txt'
+
+with open(readme, 'r') as fh:
   long_description = fh.read()
 
 CLASSIFIERS = [
@@ -8,36 +16,29 @@ CLASSIFIERS = [
   'License :: OSI Approved :: MIT License',
 ]
 
-INSTALL_REQUIREMENTS = [
-  'GitPython==3.1.24',
-  'click',
-  'click-help-colors',
-  'regex',
-]
+with open(reqs, 'r') as f:
+  install_requirements = f.read().splitlines()
 
-DEV_REQUIREMENTS = [
-  'coverage',
-  'sphinx',
-  'sphinx_rtd_theme',
-]
+with open(reqs_dev, 'r') as f:
+  dev_requirements = f.read().splitlines()
 
 setup(name='gitmerge',
-      version='0.0.6',
+      version=__version__,
       description='Add your git commits from your corporate account to your private profile with gitmerge.',
       author='Benedikt Scheffler',
       author_email='scheffler.benedikt@gmail.com',
       license='MIT',
       long_description=long_description,
       long_description_content_type='text/markdown',
-      packages=['gitmerge'],
+      packages=find_packages(),
       classifiers=CLASSIFIERS,
       entry_points={
         'console_scripts': ['gitmerge=gitmerge.gitmerge:gitmerge']
       },
       # prod dependencies
-      install_requires=INSTALL_REQUIREMENTS,
+      install_requires=install_requirements,
       # dev dependencies
       extras_require={
-        'dev': DEV_REQUIREMENTS
+        'dev': dev_requirements
       },
       python_requires='>=3.8')
